4e771802c797fa8430a20e057f762fb711d6bffa
Semen Simanov <63959715+bpatapb88@users.noreply.github.com>
2020-04-19 15:11:17 +0200

    Add kata.sh

---
+++ kata.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+a=5
+b=1
+c=3
+
+i=$(($a+$b+$c))
+j=$(($a*$b*$c))
+k=$(($a+$b*$c))
+l=$((($a+$b)*$c))
+n=$(($a*($b+$c)))
+m=0
+for result in $i $j $k $l $n
+do
+    if [ $m -lt $result ] 
+    then
+    m=$result
+    fi
+done
+echo "$m"
+
+mydir=/home/simon/Documents/likegeeks
+
+if [ -d $mydir ]
+then
+echo "The $mydir directory exists"
+
+cd $mydir
+ls
+
+else
+echo "The $mydir directory does not exist"
+fi
